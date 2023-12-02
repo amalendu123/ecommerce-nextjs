a15cd697081d378c6b7b31e75def5fbb9b12e22b
AMALENDU MANOJ <220229@tkmce.ac.in>
2023-12-02 14:53:56 +0530

    added navbar and footer

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='h-40 p-10 flex gap-10 bg-black text-white justify-between'>
+        <div className='text-xl break-all'>
+            A online site to sell merchandise for women,kids and mens
+        </div>
+        <div className=''>
+            <p>Social media detail</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='flex justify-center space-x-10 bg-cyan-500 h-14 p-4'>
+        <Link href={"/"} className='hover:text-white hover:text-3xl transition-all duration-300'>Home</Link>
+        <Link href={"/kids"} className='hover:text-white hover:text-3xl transition-all duration-300'>Kids wear</Link>
+        <Link href={"/ladies"} className='hover:text-white hover:text-3xl transition-all duration-300'>Ladies wear</Link>
+        <Link href={"/mens"} className='hover:text-white hover:text-3xl transition-all duration-300'>Mens</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
